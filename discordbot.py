6d9a297cc6cedde661014b879ca823fe3de1f24d
Yusuke Mizuno <poke4202015@gmail.com>
2021-11-19 02:18:53 +0900

    Add function of voice entry system

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ from os import getenv
 import traceback
 
 bot = commands.Bot(command_prefix='/')
-
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -15,6 +15,18 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+# チャンネル入退室時の通知処理
+@client.event
+async def on_voice_state_update(member, before, after):
+
+    # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
+        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+        botRoom = client.get_channel(TEXT_ID)
+
+        # 入室通知（画面共有に反応しないように分岐）
+        if after.channel is not None and after.channel is not before.channel:
+            await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しました！")
 
 
 token = getenv('DISCORD_BOT_TOKEN')
